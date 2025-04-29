1ba2b1f542c58c451ef5b8689cf9dcd76de4bf2f
chandan <chandan868@gmail.com>
2025-04-29 12:18:17 +0530

    useState counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import { Typography } from '@mui/material'
+import { Button, Card, Typography } from '@mui/material'
 import './App.css'
+import { useState } from 'react'
 
-function App() {
-
+export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <Typography variant='h3'>Welcome to Counter App!</Typography>      
+    <Card variant='elevation' style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: 30
+    }}>
+      <Typography variant='h3'>Welcome to Counter App!</Typography>
+      <Buttons count={count} setCount={setCount} />
+      <CountComponent count={count} />
+    </Card>
+  )
+}
+
+function Buttons({count, setCount}) {
+  return (
+    <div className='buttons' style={{
+      display: "flex",
+      gap: 40
+    }}>
+      <Increase count={count} setCount={setCount} />
+      <Decrease count={count} setCount={setCount} />
     </div>
   )
 }
 
-export default App
+function Increase({count, setCount}) {
+  return (
+    <Button variant='contained' onClick={()=>{
+      setCount(count+1)
+    }}    
+    >
+      Increase
+    </Button>
+  )
+}
+
+function Decrease({count, setCount}) {
+  return (
+    <Button variant='contained' onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</Button>
+  )
+}
+
+function CountComponent({count}) {
+  return (
+    <Typography variant='h5'>{count}</Typography>
+  )
+}
