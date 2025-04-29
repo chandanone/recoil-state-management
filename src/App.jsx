f4cfc4eff72e63d7e0c70065f570aaa9faddfa78
chandan <chandan868@gmail.com>
2025-04-29 11:13:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Typography variant='h3'>Welcome to Counter App!</Typography>      
+    </div>
+  )
+}
+
+export default App
